439a104de12a96d7e44adea323c451a843e763bc
Abtswiath丶 <longxiying@vip.qq.com>
2017-10-12 17:39:04 +0800

    fuck

--- app/src/main/java/com/abtswiath/Path1Activity.java
+++ app/src/main/java/com/abtswiath/Path1Activity.java
@@ -1,18 +1,39 @@
 package com.abtswiath;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.abtswiath.base.BaseActivity;
+import com.abtswiath.view.View2;
 
 /**
  * Created by longx on 2017/10/12.
  */
 
-public class Path1Activity extends BaseActivity{
+public class Path1Activity extends BaseActivity implements View.OnClickListener {
+
+    private TextView title;
+    private View2 view2;
+    private static final String TAG = "Path1Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_path1);
+        view2 = (View2) this.findViewById(R.id.view);
+        title = (TextView) this.findViewById(R.id.tv_title);
+        title.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_title:
+                Log.e(TAG, "onClick: tv_title" );
+                view2.change();
+                break;
+        }
     }
 }
--- app/src/main/java/com/abtswiath/view/View2.java
+++ app/src/main/java/com/abtswiath/view/View2.java
@@ -3,9 +3,11 @@ package com.abtswiath.view;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Path;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
 
 /**
  * Created by longx on 2017/10/12.
@@ -27,10 +29,29 @@ public class View2 extends BaseView {
         super(context, attrs, defStyleAttr);
     }
 
-    int i = 20;
+    private int i = 20;
 
     @Override
     protected void onDraw(Canvas canvas) {
+        Log.e(TAG, "onDraw: ");
+        switch (index) {
+            case 0:
+                pathLine(canvas);
+                break;
+            case 1:
+                pathAddCircle(canvas);
+                break;
+        }
+    }
+
+    private void pathAddCircle(Canvas canvas) {
+        canvas.translate(getWidth() / 2, getHeight() / 2);
+        Path path = new Path();
+        path.addCircle(0, 0, 40, Path.Direction.CW);
+        canvas.drawPath(path, mPaint);              // 绘制Path
+    }
+
+    private void pathLine(Canvas canvas) {
         canvas.translate(getWidth() / 2, getHeight() / 2);
         Path path = new Path();
         while (i < 300) {
@@ -44,6 +65,18 @@ public class View2 extends BaseView {
         canvas.drawPath(path, mPaint);              // 绘制Path
     }
 
+
+    private int index = 0;
+
+    public void change() {
+        if (index > 2) {
+            index = 0;
+        } else {
+            index++;
+        }
+        Log.e(TAG, "change: index=" + index);
+        invalidate();
+    }
 
     private int j = 20;
 
