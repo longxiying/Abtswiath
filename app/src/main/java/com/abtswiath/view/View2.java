9f221ba8c5f4d1d25557b25706c37d61e75aed7a
Abtswiath丶 <longxiying@vip.qq.com>
2017-10-12 17:07:15 +0800

    update

--- app/src/main/java/com/abtswiath/view/View2.java
+++ app/src/main/java/com/abtswiath/view/View2.java
@@ -4,12 +4,17 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Path;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 
 /**
  * Created by longx on 2017/10/12.
  */
 
 public class View2 extends BaseView {
+
+    private static final String TAG = "View2";
+
     public View2(Context context) {
         super(context);
     }
@@ -22,20 +27,44 @@ public class View2 extends BaseView {
         super(context, attrs, defStyleAttr);
     }
 
+    int i = 20;
+
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.translate(getWidth() / 2, getHeight() / 2);
         Path path = new Path();
-        int i = 20;
         while (i < 300) {
             path.moveTo(0, i);
             path.lineTo(i, 0);
             path.lineTo(0, -i);
             path.lineTo(-i, 0);
             path.close();
-            i = i + 20;
+            i = i + j;
         }
         canvas.drawPath(path, mPaint);              // 绘制Path
+    }
 
+
+    private int j = 20;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int rawX = (int) event.getRawX();
+                int rawY = (int) event.getRawY();
+                i = (rawX + 3) / 3;
+                if (j == i) {
+                    return true;
+                }
+                j = i;
+                Log.e(TAG, "rawX: " + rawX + "rawY：" + rawY);
+
+                invalidate();
+                break;
+        }
+        return true;
     }
 }
